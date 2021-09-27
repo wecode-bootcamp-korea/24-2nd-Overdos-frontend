e0614235f7d92d22d61c95b90c992ebadec8ee02
youngho <youngho8762@gamil.com>
2021-09-27 10:49:31 +0900

    Add : nav 레이아웃 완료 Add : 로그인 로그아웃 기능 추가

--- src/Routes.js
+++ src/Routes.js
@@ -4,10 +4,12 @@ import Footer from './components/Footer/Footer';
 import Main from './pages/Main/Main';
 import SignUp from './pages/Sign/SignUp/SignUp';
 import SignIn from './pages/Sign/SignIn/SignIn';
+import Nav from './components/Nav/Nav';
 
 const Routes = () => {
   return (
     <Router>
+      <Nav />
       <Switch>
         <Route exact path="/" component={Main} />
         <Route exact path="/signup" component={SignUp} />
