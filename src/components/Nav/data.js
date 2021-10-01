27c012f49c4cde3f71119aa7889ca60c6d1a57b9
youngho <youngho8762@gamil.com>
2021-10-01 10:21:31 +0900

    Modify : nav 라우트 주소에 맞는 history.push 적용

--- src/components/Nav/data.js
+++ src/components/Nav/data.js
@@ -2,22 +2,22 @@ export const MENU_LIST = [
   {
     id: 1,
     title: '추천성분',
-    push: '/추천성분',
+    push: '/survey',
   },
   {
     id: 2,
     title: '제품보기',
-    push: '/제품보기',
+    push: '/product',
   },
   {
     id: 3,
     title: '고객후기',
-    push: '/고객후기',
+    push: '/review',
   },
   {
     id: 4,
     title: '장바구니',
-    push: '/장바구니',
+    push: '/cart',
   },
   {
     id: 5,
